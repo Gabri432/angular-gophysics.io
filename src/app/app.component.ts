173ad0e1e6419e9274999c531d7d0ad677bbfacb
Gabri432 <gabrielegatti432@gmail.com>
2023-05-28 19:23:13 +0200

    Hiding sidebar when opening website on main page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-gophysics-io';
+  hideComponent = false;
+
+  constructor(private router: Router) {
+    router.events.subscribe((route) => {
+      if (route instanceof NavigationEnd) {
+        if (route.url == "/") {
+          this.hideComponent = true;
+        }
+      }
+    })
+  }
 }
