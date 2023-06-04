c202d0c47fe408343d1548a0e517bcdc771f9c5b
Gabri432 <gabrielegatti432@gmail.com>
2023-06-04 18:22:59 +0200

    Creating the 'documentation-page' component.

--- src/app/features/documentation/documentation-page/documentation-page.component.ts
+++ src/app/features/documentation/documentation-page/documentation-page.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+import { Function } from '../../../core/models/function';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-documentation-page',
   templateUrl: './documentation-page.component.html',
   styleUrls: ['./documentation-page.component.scss']
 })
 export class DocumentationPageComponent {
+
+  private _jsonURL = 'assets/database/functions.json';
+  functions: Array<Function> = [];
+  products: any = [];
+
+  constructor(public http: HttpClient) {
+  };
+
+  ngOnInit() {
+    this.loadFunctions().subscribe(data =>{
+      console.log(data);
+      this.products = data;
+      this.functions = data;
+      console.log(this.functions);
+    })
+  }
+
+  loadFunctions(): Observable<Function[]> {
+    return this.http.get<Function[]>((this._jsonURL))
+  }
 
 }
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Function } from '../../core/models/function';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -9,32 +6,5 @@ import { Observable } from 'rxjs';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  functionLibrary: Function = {
-    id: 1,
-    functionName: 'Force',
-    functionDescription: 'The Force is the product of the mass and acceleration.',
-    functionFormula: 'F=m*a',
-    context: 'Classic Physics'
-  };
-
-  private _jsonURL = 'assets/database/functions.json';
-  functions: Array<Function> = [];
-  products: any = [];
-
-  constructor(public http: HttpClient) {
-  };
-
-  ngOnInit() {
-    this.loadFunctions().subscribe(data =>{
-      console.log(data);
-      this.products = data;
-      this.functions = data;
-      console.log(this.functions);
-    })
-  }
-
-  loadFunctions(): Observable<Function[]> {
-    return this.http.get<Function[]>((this._jsonURL))
-  }
 
 }
