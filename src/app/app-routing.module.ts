4a38675403967e0845629ced7598408960a6d025
Gabri432 <gabrielegatti432@gmail.com>
2023-09-12 12:00:53 +0200

    No longer using 'Error404component'.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Error404Component } from './core/components/error404/error404.component';
+// import { Error404Component } from './core/components/error404/error404.component';
 import { HomeComponent } from './features/home/home.component';
 import { DocumentationPageComponent } from './features/documentation/documentation-page/documentation-page.component';
 import { AboutPageComponent } from './features/about/about-page/about-page.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, title: 'Home Page'},
+  {path: '**', component: HomeComponent, title: 'Home Page'},
   {path: 'documentation', component: DocumentationPageComponent, title: 'Gophysics Documentation'},
   {path: 'about', component: AboutPageComponent, title: 'About gophysics'},
-  {path: '**', component: Error404Component, title: 'Error Page Not Found'}
+  //{path: '**', component: Error404Component, title: 'Error Page Not Found'}
 ];
 
 @NgModule({
