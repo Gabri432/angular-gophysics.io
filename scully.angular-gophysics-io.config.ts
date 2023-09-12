5df2e1aa51b50164daa40723435a1a95b38b6603
Gabri432 <gabrielegatti432@gmail.com>
2023-09-12 15:54:58 +0200

    Installing and using Scully.

---
+++ scully.angular-gophysics-io.config.ts
@@ -0,0 +1,10 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'angular-gophysics-io',
+  distFolder: './docs', // output directory of your Angular build artifacts
+  outDir: './docs', // directory for scully build artifacts
+  defaultPostRenderers: [],
+  routes: {},
+};
