0f28b6a106a1355171cb7f80ac2720d1545b10bf
Gabri432 <gabrielegatti432@gmail.com>
2023-09-18 20:15:37 +0200

    Changing output folder for angular builds.

--- scully.angular-gophysics-io.config.ts
+++ scully.angular-gophysics-io.config.ts
@@ -3,7 +3,7 @@ import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: './src',
   projectName: 'angular-gophysics-io',
-  distFolder: './docs', // output directory of your Angular build artifacts
+  distFolder: './dist', // output directory of your Angular build artifacts
   outDir: './docs', // directory for scully build artifacts
   defaultPostRenderers: [],
   routes: {},
